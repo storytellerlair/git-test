b7834f525b184e104b91e79a59a015717ebfda2f
haiyang <604359773@qq.com>
2022-01-16 15:07:26 +0800

    luanqbz

---
+++ git-test/src/main/java/com/haiyang/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.haiyang.git;
+
+/**
+ * @author 杨磊
+ * @version 1.0.0
+ * @ClassName GitTest.java
+ * @Description TODO
+ * @createTime 2022年01月16日 15:02:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
